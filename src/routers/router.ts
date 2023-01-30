0e188a82e26adc4a283e0ea27c9db101996081cb
Junim <dbaudwns20@gmail.com>
2023-01-30 23:06:05 +0900

    라우팅 기능 개선
    - 없는 페이지인 경우 체크
    - 비정상접근 방지
    - 로그인이 필요한 페이지인지 체크
    - 로그인한 상태에서 url 입력으로 로그인 페이지 이동할 경우 대시보드로 이동 -> 자동로그인 처리

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import { getSessionToken } from '@/stores/store'
 import authRouter, { SignInNormal } from './auth.router'
-import dashboardRouter from './dashboard.router'
+import dashboardRouter, { Dashboard } from './dashboard.router'
 import classRouter from './class.router'
 import utils from "@/utils/utils"
 
@@ -25,11 +25,18 @@ const router = createRouter({
 })
 
 /**
- * 접근하는 라우터에 접근할 수 있는지 체크
- * 토큰여부를 확인하여 페이지 라우팅, 없는 경우 로그인 페이지로 리다이렉트
+ * router 이동 시 체크사항
  */
 router.beforeEach((to, from, next) => {
-  // router.meta.accessiblePaths 에 정의된 경로만 이동되도록
+  // 없는 페이지인 경우
+  // router 의 meta.needLogin 은 필수요소, 없다면 존재하지 않는 컴포넌트 뷰로 취급한다
+  if (_.isUndefined(to.meta.needLogin)) {
+    utils.message.showErrorToastMsg('존재하지 않는 페이지입니다')
+    next(from) // 현재페이지로 이동
+    return
+  }
+  // 비정상접근 방지
+  // router 의 meta.accessiblePaths 가 정의되어 있다면 해당 경로의 from 으로 이동되었을 때만 허용한다
   if (!_.isUndefined(to.meta.accessiblePaths)) {
     let isAccessible: boolean = false
     const paths: string[] = to.meta.accessiblePaths as string[]
@@ -45,8 +52,9 @@ router.beforeEach((to, from, next) => {
     }
   }
   // 로그인이 필요한 페이지인지 체크
+  // router 의 meta.needLogin 은 필수요소, 참이라면 로컬스토리지에 토큰이 있는지 확인한다
+  const isLogin: boolean = getSessionToken() != null
   if (to.meta.needLogin) {
-    const isLogin: boolean = getSessionToken() != null
     if (!isLogin) {
       utils.message.showWarningToastMsg('로그인이 필요합니다')
       next(from) // 현재페이지로 이동
@@ -54,7 +62,12 @@ router.beforeEach((to, from, next) => {
       next()
     }
   } else {
-    next()
+    // 로그인한 상태에서 url 입력으로 로그인 페이지 이동할 경우 대시보드로 이동 -> 자동로그인 처리
+    if (isLogin && to.path === SignInNormal.path) {
+      next(Dashboard)
+    } else {
+      next()
+    }
   }
 })
 
