8d9ae040e1b028782673f0f65f580a1f8dea69ed
Junim <dbaudwns20@gmail.com>
2023-03-12 16:34:49 +0900

    #58 DateRangePicker 추가

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,3 +3,6 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+// declare bulma-calendar
+declare module 'bulma-calendar/dist/js/bulma-calendar.min.js'
